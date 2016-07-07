115c07c3b78a7e481f14106b0c73f83af6c6f2a1
xbt18795959581 <xbt18795959581@163.com>
2016-07-07 22:34:09 +0800

    xbt

---
+++ img/node.js
@@ -0,0 +1,9 @@
+/**
+ * Created by XUBAOTING on 2016/7/5.
+ */
+console.log('hello world')
+//node node node node node node xbt xbtxbt xbt xvt xvt xbr xvr xbt xbt wgdwgd wgd wgd wgd/*
+/*
+* 我的打字速度真的是太慢了我还要多多练习
+*
+* */
